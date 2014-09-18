fd946e412714eab178806245ff44bc5ef9f3dd36
Alan <marchanalan@hotmail.com>
2014-09-18 14:36:48 -0300

    Borro las carpetas que estan de mas.

--- app/views/default/sections/s.header.php
+++
@@ -1,4 +0,0 @@
-<div style=" margin:20px 0 0 0; text-align:center;">
-	<h1>Model, View and Controller - by Mouse</h1>		 			
-	<h1>#TITLE#</h1>
-</div>
--- app/views/default/sections/s.menuizquierda.php
+++
@@ -1,7 +0,0 @@
-<div id="menu">
-  <ul>
-    <li><a href="index.php" >Principal</a></li>
-    <li><a href="index.php?action=buscar">Buscar</a></li>
-    <li><a href="index.php?action=history">History of Bolivia</a></li>
-  </ul>
-</div>
