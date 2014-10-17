031f872018746e1327d6931dfa266105bbff88ce
valde1992 <fernando_valde@hotmail.com>
2014-10-17 17:05:39 -0300

    Se crea la base de la vista listado

---
+++ vista/modulos/mostrarListado.php
@@ -0,0 +1,43 @@
+<!--
+
+  Como jefe del Dpto Alumnos necesito poder emitir un listado 
+  alfabético de alumnos por cursada o por comisión.
+
+- Necesito poder sacar el listado en formato PDF.
+- Necesito que se muestre el total de almnos incluidos en el listado.
+- En el listado por cursada, si hay más de una comision indicar 
+  para cada alumno la comision en la que esta inscripto.
+
+-->
+
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <title>Listado de alumnos</title>
+ 
+    <link href="../../librerias/css/bootstrap.min.css" rel="stylesheet" media="screen">
+     <link href="../../librerias/datepicker/css/datepicker.css" rel="stylesheet" media="screen">
+     <script language = 'JavaScript' src="../../librerias/js/jquery-1.11.1.min.js"></script>
+     <script language = 'JavaScript' src="../../librerias/js/bootstrap.min.js"></script>
+    <script language = 'JavaScript' src="../../librerias/datepicker/js/bootstrap-datepicker.js"></script>
+	</head>
+	<br>
+	<br>
+	<body>
+		<div class = 'container'>
+			<ol class = 'breadcrumb'>
+				<li><a href = '../index.php'>Inicio</a></li>
+				<li class = 'active'>Ver listado</li>
+			</ol>
+		</div>
+		<div class = 'container'>
+			<div class="col-sm-4">
+				<fieldset>
+					
+				</fieldset>
+			</div>
+		</div>
+	</body>
+</html>
+
