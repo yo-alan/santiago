626e680c315574a4c9536b2de97935934b600fdd
Alan <marchanalan@hotmail.com>
2014-10-23 20:34:55 -0300

    Creo el modelo de la clase, con sus getters y setters.

--- modelo/clase.class.php
+++ modelo/clase.class.php
@@ -37,20 +37,20 @@ class Clase {
 	
 	//INICIO METODOS ESTATICOS
 	
-	static function clase($id_comision){
+	static function clase($id_clase){
 		//METODO ESTATICO QUE DEVUELVE UNA CLASE ESPECIFICA
 		
 		$c = new Clase();
 		
 		$conn = new Conexion();
 		
-		$sql = 'SELECT * FROM clase WHERE id_comision = :id_comision';
+		$sql = 'SELECT * FROM clase WHERE id_clase = :id_clase';
 		
 		$consulta = $conn->prepare($sql);
 		
 		$consulta->setFetchMode(PDO::FETCH_ASSOC);
 		
-		$consulta->bindParam(':id_comision', $id_comision, PDO::PARAM_INT);
+		$consulta->bindParam(':id_clase', $id_clase, PDO::PARAM_INT);
 		
 		try{
 			
@@ -60,7 +60,17 @@ class Clase {
 			
 			$c->nuevo = false;
 			$c->cambios = false;
-			$c->id_comision = $results['id_comision'];
+			$c->id_clase = $results['id_clase'];
+			$c->obligatorio = $results['obligatorio'];
+			$c->hora_inicio = $results['hora_inicio'];
+			$c->hora_fin = $results['hora_fin'];
+			$c->aula = $results['aula'];
+			$c->dictada = $results['dictada'];
+			$c->recuperatoria_de = $results['recuperatoria_de'];
+			$c->comision = $results['comision'];
+			$c->profesor = $results['profesor'];
+			$c->hora_ingreso_profesor = $results['hora_ingreso_profesor'];
+			$c->hora_salida_profesor = $results['hora_salida_profesor'];
 			
 		}catch(PDOException $e){
 			
@@ -91,15 +101,21 @@ class Clase {
 		if($this->nuevo){
 			
 			try{
-				$sql = 'INSERT INTO clase(carrera, materia, anio, numero)
-						VALUES(:carrera, :materia, :anio, :numero)';
+				$sql = 'INSERT INTO clase(obligatorio, hora_inicio, hora_fin, aula, dictada, recuperatoria_de, comision, profesor, hora_ingreso_profesor, hora_salida_profesor)
+						VALUES(:obligatorio, :hora_inicio, :hora_fin, :aula, :dictada, :recuperatoria_de, :comision, :profesor, :hora_ingreso_profesor, :hora_salida_profesor)'
 				
 				$consulta = $conn->prepare($sql);
-				  
-				$consulta->bindParam(':carrera', $this->carrera, PDO::PARAM_STR);
-				$consulta->bindParam(':materia', $this->materia, PDO::PARAM_INT);
-				$consulta->bindParam(':anio', $this->anio, PDO::PARAM_INT);
-				$consulta->bindParam(':numero', $this->numero, PDO::PARAM_INT);
+				
+				$consulta->bindParam(':obligatorio', $this->obligatorio, PDO::PARAM_INT);
+				$consulta->bindParam(':hora_inicio', $this->hora_inicio, PDO::PARAM_STR);
+				$consulta->bindParam(':hora_fin', $this->hora_fin, PDO::PARAM_STR);
+				$consulta->bindParam(':aula', $this->aula, PDO::PARAM_STR);
+				$consulta->bindParam(':dictada', $this->dictada, PDO::PARAM_INT);
+				$consulta->bindParam(':recuperatoria_de', $this->recuperatoria_de, PDO::PARAM_INT);
+				$consulta->bindParam(':comision', $this->comision, PDO::PARAM_INT);
+				$consulta->bindParam(':profesor', $this->profesor, PDO::PARAM_INT);
+				$consulta->bindParam(':hora_ingreso_profesor', $this->hora_ingreso_profesor, PDO::PARAM_STR);
+				$consulta->bindParam(':hora_salida_profesor', $this->hora_salida_profesor, PDO::PARAM_STR);
 				
 				$consulta->execute();
 				
@@ -121,19 +137,139 @@ class Clase {
 		if($this->nuevo)
 			return;
 		
-		$sql = "DELETE FROM clase WHERE id_comision = :id_comision";
+		$sql = "DELETE FROM clase WHERE id_clase = :id_clase";
 		
 		$consulta = $conn->prepare($sql);
 		
-		$consulta->bindParam(':id_comision', $this->id_comision, PDO::PARAM_INT);
+		$consulta->bindParam(':id_clase', $this->id_clase, PDO::PARAM_INT);
 		
 		try{
 			
 			$consulta->execute();
 			
 		}catch(PDOException $e){
-			throw new Exception('Error al eliminar la comision: '.$e->getMessage());
+			throw new Exception('Error al eliminar la clase: '.$e->getMessage());
 		}
 		
+	}
+	
+	function getObligatorio(){
+		return $this->obligatorio;
+	}
+	
+	function setObligatorio($obligatorio){
+		
+		//VALIDACIONES
+		
+		$this->obligatorio = $obligatorio;
+		$this->cambios = true;
+	}
+	
+	function getHora_inicio(){
+		return $this->hora_inicio;
+	}
+	
+	function setHora_inicio($hora_inicio){
+		
+		//VALIDACIONES
+		
+		$this->hora_inicio = $hora_inicio;
+		$this->cambios = true;
+	}
+	
+	function getHora_fin(){
+		return $this->hora_fin;
+	}
+	
+	function setHora_fin($hora_fin){
+		
+		//VALIDACIONES
+		
+		$this->hora_fin = $hora_fin;
+		$this->cambios = true;
+	}
+	
+	function getAula(){
+		return $this->aula;
+	}
+	
+	function setAula($aula){
+		
+		//VALIDACIONES
+		
+		$this->aula = $aula;
+		$this->cambios = true;
+	}
+	
+	function getDictada(){
+		return $this->dictada;
+	}
+	
+	function setDictada($dictada){
+		
+		//VALIDACIONES
+		
+		$this->dictada = $dictada;
+		$this->cambios = true;
+	}
+	
+	function getRecuperatoria_de(){
+		return $this->recuperatoria_de;
+	}
+	
+	function setRecuperatoria_de($recuperatoria_de){
+		
+		//VALIDACIONES
+		
+		$this->recuperatoria_de = $recuperatoria_de;
+		$this->cambios = true;
+	}
+	
+	function getComision(){
+		return $this->comision;
+	}
+	
+	function setComision($comision){
+		
+		//VALIDACIONES
+		
+		$this->comision = $comision;
+		$this->cambios = true;
+	}
+	
+	function getProfesor(){
+		return $this->profesor;
+	}
+	
+	function setProfesor($profesor){
+		
+		//VALIDACIONES
+		
+		$this->profesor = $profesor;
+		$this->cambios = true;
+	}
+	
+	function getHora_ingreso_profesor(){
+		return $this->hora_ingreso_profesor;
+	}
+	
+	function setHora_ingreso_profesor($hora_ingreso_profesor){
+		
+		//VALIDACIONES
+		
+		$this->hora_ingreso_profesor = $hora_ingreso_profesor;
+		$this->cambios = true;
+	}
+	
+	function getHora_salida_profesor(){
+		return $this->hora_salida_profesor;
+	}
+	
+	function setHora_salida_profesor($hora_salida_profesor){
+		
+		//VALIDACIONES
+		
+		$this->hora_salida_profesor = $hora_salida_profesor;
+		$this->cambios = true;
 	}
 	
