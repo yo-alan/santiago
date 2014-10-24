0aa488c25e77c9b283081f2f419bd93a8655f696
valde1992 <fernando_valde@hotmail.com>
2014-10-24 19:58:58 -0300

    cambio menor

--- vista/modulos/mostrarListado.php
+++ vista/modulos/mostrarListado.php
@@ -29,6 +29,7 @@
 	<body>
 		<div class = "container">
 			<ol class = 'breadcrumb'>
+				<li><a href = '../index.php'>Inicio</a></li>
 				<li class = 'active'>Ver listado</li>
 			</ol>
 		<form method = 'POST' action = '../../controlador/comision.php' class = 'form-horizontal' role = 'form'>
