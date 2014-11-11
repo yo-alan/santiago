1debcaa48a61bd71ed0fd6b6dd260dade3a9f239
Nestor <nestorsandovalsandoval@gmail.com>
2014-11-11 11:07:56 -0300

    Vista para busqueda de comision Arreglada

--- vista/modulos/list-comision.php
+++ vista/modulos/list-comision.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>Bedelía | Listado de comisiones</title>
+		<title>Bedel&iacute;a | Listado de comisiones</title>
 		<link rel="Stylesheet" href="../librerias/css/bootstrap.css">
 		<link rel="Stylesheet" href="../librerias/css/default.css">
         <link rel="stylesheet" href="../librerias/css/jquery.dataTables.css">
@@ -8,34 +8,44 @@
 		<script src="../librerias/js/jquery.js"></script>
 		<script src="../librerias/js/bootstrap.js"></script>
 		<script src="../librerias/js/twitter-bootstrap-hover-dropdown.min.js"></script>
-         <script src="../librerias/js/jquery.dataTables.min.js"></script>
+        <script src="../librerias/js/jquery.dataTables.min.js"></script>
         <script src="../librerias/js/listadosTabla.js"></script>
 	</head>
-<ol class = 'breadcrumb'>
-    <li><a href = '../index.php'>Inicio</a></li>
-    <li class = 'active'>Listado de Comisiones</li>	
-</ol>
-<div class="jumbotron">
-    <table class="table table-striped tablaData">
-        <thead>
-            <tr>
-                <th>Cod.Comision</th>
-                <th>Cod. Carrera</th>
-                <th>Materia</th>
-                <th>Anio</th>
-                <th>Nro Comision</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($cp as $c):?>
-            <tr>
-                <td><?php echo $c->getId_comision()?></td>
-                <td><?php echo $c->getCarrera()?></td>
-                <td><?php echo $c->getNombre_materia()?></td>
-                <td><?php echo $c->getAnio()?></td>
-                <td><?php echo $c->getNumero()?></td>
-                <td>
-            <?php endforeach;?>
-        </tbody>
-    </table>    
+    <body>
+        <div class="container">
+            <ol class = 'breadcrumb'>
+                <li><a href = '../index.php'>Inicio</a></li>
+                <li class = 'active'>Listado de Comisiones</li>	
+            </ol>
+        </div>
+        <div class="container">
+            <div class="jumbotron">
+                <table class="table table-striped tablaData">
+                    <thead>
+                        <tr>
+                            <th>Cod.Comision</th>
+                            <th>Cod. Carrera</th>
+                            <th>Materia</th>
+                            <th>Anio</th>
+                            <th>Nro Comision</th>
+                            <th>Enlazar Alum.</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($cp as $c):?>
+                        <tr>
+                            <td><?php echo $c->getId_comision()?></td>
+                            <td><?php echo $c->getCarrera()?></td>
+                            <td><?php echo $c->getNombre_materia()?></td>
+                            <td><?php echo $c->getAnio()?></td>
+                            <td><?php echo $c->getNumero()?></td>
+                            <td>
+                                <a href="?action=enlazarAlumno"><i class="glyphicon glyphicon-list-alt"></i></a>
+                            </td>
+                        <?php endforeach;?>
+                    </tbody>
+                </table>    
+            </div>
+        </div>
+    </body>
 </html>
