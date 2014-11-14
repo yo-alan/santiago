5369fdbb2c6186aa60e63edc20f9a1f49bc23409
Nestor <nestorsandovalsandoval@gmail.com>
2014-11-14 18:02:10 -0300

    arreglo vista de formularios

--- vista/modulos/form-alumno.php
+++ vista/modulos/form-alumno.php
@@ -4,38 +4,40 @@
 	<body>
 		<?php include '../vista/menu.php';?>
 		<div class="container">
-			<div class = 'jumbotron'>
-				<form role="form" class="form-horizontal col-lg-2" method="post" action="../controlador/alumno.php">
+			<div class = 'container jumbotron'>
+                <div class="col-md-12">
+				<form role="form"  method="post" action="../controlador/alumno.php">                    
 					<fieldset>
 						<legend>Alta Alumno</legend>
 						<input type="hidden" name="action" value="agregar">
-						<div class="form-group">
+						<div class="form-group col-md-6">
 							<label class="control-label">Apellido:</label>
 							<input type="text" class="form-control" name="apellido">
 						</div>
-						<div class="form-group">
+						<div class="form-group col-md-6">
 							<label class="control-label">Nombre:</label>
 							<input type="text" class="form-control" name="nombre">
 						</div>
-						<div class="form-group">
+						<div class="form-group col-md-6">
 							<label class="control-label">D.N.I.:</label>
 							<input type="text" class="form-control" name="documento">
 						</div>
-						<div class="form-group">
+						<div class="form-group col-md-6">
 							<label class="control-label">Fecha Nacimiento:</label>
 							<input type="datetime" class="form-control datepicker" name="f_nacimiento">
 						</div>
-						<div class="form-group">
+						<div class="form-group col-md-6">
 							<label class="control-label">Direccion:</label>
 							<input type="text" class="form-control" name="direccion">
 						</div>
-						<div class="form-group">
+						<div class="form-group col-md-6">
 							<label class="control-label">Legajo:</label>
 							<input type="text" class="form-control" name="legajo">    
 						</div>
-						<button type = 'submit' class =  'btn btn-primary col-md col-md-offset-6'>Guardar</button> 
+						<button type = 'submit' class =  'btn btn-primary col-md-2 col-md-offset-6'>Guardar</button> 
 					</fieldset>    
 				</form>
+                </div>
 			</div>
 			<script>
 				$(function () {
--- vista/modulos/form-cursada.php
+++ vista/modulos/form-cursada.php
@@ -4,59 +4,49 @@
 	<body onload = "selectMateria();">
 		<?php include '../vista/menu.php';?>
 		<div class = 'container'>
-            <div class="jumbotron">
-			<form method = 'POST' action = '../controlador/cursada.php' class = 'form-horizontal' role = 'form'>
-				<div class = 'row'>
-					<div class = 'form-group col-lg-2'>
-						<legend>Alta Cursada</legend>
+            <div class="container jumbotron">
+                <div class="col-md-12">
+                <form method = 'POST' action = '../controlador/cursada.php' role='form'>
+                    <fieldset>
+                    <legend>Alta Cursada</legend>
+					<div class = 'form-group col-md-6'>
 						<label for= 'inicio' class ='control-label'>Inicio</label>
 						<input type = 'datetime' class = 'datepicker form-control' name = 'inicio'></input>
-					</div>
-				</div>
-				<div class = 'row'>
-					<div class = 'form-group col-lg-2'>
+					</div>		
+					<div class = 'form-group col-md-6'>
 						<label for = 'fin' class = 'control-label'>Fin</label>
 						<input type = 'datetime' class = 'form-control datepicker' name = 'fin'>
-					</div>
-				</div>
-				<div class = 'row'>
-					<div class = 'form-group col-lg-2'>
+					</div>		
+					<div class = 'form-group col-md-6'>
 						<label for = 'anio' class = 'control-label'>A&ntilde;o</label>
 						<input type = 'text' class = 'form-control' name = 'anio'>
-					</div>
-				</div>
-				<div class = 'row'>
-					<div class = 'form-group col-lg-2'>
+					</div>		
+					<div class = 'form-group col-md-6'>
 						<label for = 'cuatrimestre' class = 'control-label'>Cuatrimestre</label>
 						<select class = 'form-control' name = 'cuatrimestre'>
 							<option value="1">1</option>
 							<option value="2">2</option>
 						</select>
-					</div>
-				</div>
-				<div class = 'row'>
-					<div class = 'form-group col-lg-2'>
+					</div>		
+					<div class = 'form-group col-md-6'>
 						<label for = 'carrera' class = 'control-label'>Carrera</label>
-						<select class = 'form-control' name = 'carrera' id = 'carr' onchange = "selectMateria();">
+						<select class='form-control' name='carrera' id='carr' onchange="selectMateria();">
 							<option></option>
 							<option value="SFW">Tecnicatura en Software</option>
 							<option value="RED">Tecnicatura en Redes</option>
 							<option value="ENF">Tecnicatura en Enfermer&iacute;a</option>
 						</select>
-					</div>
-				</div>
-				<div class = 'row'>
-					<div class = 'form-group col-lg-2'>
+					</div>		
+					<div class = 'form-group col-md-6'>
 						<label for = 'materia' class = 'control-label'>Materia</label>
 						<select class = 'form-control' name = 'materia' id = 'mat'>
 						</select>
 					</div>
-				</div>
-					<input type = 'hidden' name='action' value="agregar">
-					<button type = 'submit' class =  'btn btn-primary col-md col-md-offset-2'>Enviar</button>
-				<div>
-			</div>
-		</form>
+					<input type='hidden' name='action' value="agregar">
+					<button type='submit' class='btn btn-primary col-md-2 col-md-offset-6'>Enviar</button>
+                    </fieldset>
+                </form>
+            </div>
         </div>	
     </div>
 		<script>
