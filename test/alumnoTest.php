3c0ad3c68926030dabeb5e9906acaa96fce57290
radfer <maruchiin@yahoo.com.ar>
2014-11-18 10:46:11 -0300

    Nuevo test que verifica la cantidad de alumnos por comisión

--- test/alumnoTest.php
+++ test/alumnoTest.php
@@ -37,6 +37,25 @@
 				}
 			}
 		}
+		
+		public function testNoMasDe30(){
+			/*Verifica que no haya más de 30 alumnos por comisión*/
+			
+			foreach($this->alumnos as $objeto)
+			{
+				$contador = 0;
+				$comision = $objeto->getComision();
+				foreach($this->alumnos as $objeto)
+				{
+					$comision2 = $objeto->getComision();
+					if($comision == $comision2)
+					{
+						$contador++;
+					}
+				}
+				$this->assertLessThanOrEqual(30, $contador);
+			}
+		}
 	}
 
 ?>
